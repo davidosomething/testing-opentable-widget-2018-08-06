e438ea3562375c2da4d3a26cdc9534905f9aaf25
David O'Trakoun <dotrakoun@squarespace.com>
2018-08-06 14:39:08 -0400

    initial

---
+++ s.js
@@ -0,0 +1,7 @@
+function page1() {
+  page.innerHTML = "<p>this is page 1</p>" +
+    "<script type='text/javascript' src='//www.opentable.com/widget/reservation/loader?rid=983923&type=standard&theme=standard&iframe=false&overlay=false&domain=com&lang=en-US'></script>";
+}
+function page2() {
+  page.innerHTML = 'this is page 2';
+}
